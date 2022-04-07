921e7704b5e30433dec28938c9b8d2bcdc27aa6a
Jonathan Siu <jsiukob@gmail.com>
2022-04-07 11:52:42 -0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Instagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      {/* Header */}
+      <Header />  
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  )
+}
+
+export default Home
