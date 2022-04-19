e5a3d69eb13064997b38f3dd7d12444f0e89b7d5
Jonathan Siu <jonathansiu@Admins-MacBook-Pro.local>
2022-04-18 18:46:16 -0700

    add google auth, suggestions and env variables

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,11 @@ import Image from 'next/image';
 import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, ChatIcon, MenuIcon } from '@heroicons/react/outline';
 import { HomeIcon } from '@heroicons/react/solid';
 import React from 'react';
+import { useSession } from 'next-auth/react'
 
 function Header () {
+	const {data: session} = useSession();
+
 	return (
 		<div className="shadow- border-b bg-white sticky top-0 z-50 ">
 			<div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -46,7 +49,7 @@ function Header () {
 					<HeartIcon className="navBtn" />
 
 					<img
-						src="https://links.papareact.com/ocw"
+						src={session.user?.image}
 						alt="profile pic"
 						className="h-10 rounded-full"
 					/>
